bb525f7964c1dd251f0c90ff0780e79645373b33
Matt Conway <matt@conwaysplace.com>
2013-09-03 07:36:41 -0400

    fix rubinius naming

--- lib/named/naming.rb
+++ lib/named/naming.rb
@@ -1,5 +1,15 @@
 module Named
   module Naming
-    attr_accessor :name
+    
+    # rubinius 2.0 doesn't seem to like using attr_accessor for
+    # name, I'm guessing because of a naming conflict with @name
+    def name=(n)
+      @named_name = n
+    end
+    
+    def name
+      @named_name
+    end
+    
   end
 end
